dd96b345470af0440bc4b0b417beac7c71f6f83d
jpantonov <jpantonow@gmail.com>
2024-01-31 07:08:40 -0300

    fixed find

--- src/main/java/com/mongodb/db/Read.java
+++ src/main/java/com/mongodb/db/Read.java
@@ -35,24 +35,26 @@ public class Read extends Connection {
                             Aggregates.group("$id",
                                     Accumulators.addToSet("name","$name"),
                                     Accumulators.addToSet("class", "$class"),
-                                    Accumulators.addToSet("grades","$grades")
+                                    Accumulators.push("grades","$grades")
             )));
             ArrayList<Double> grades = new ArrayList<>();
             Document results = found.iterator().next();
             String name = results.get("name").toString();
             String classes = results.get("class").toString();
+            List grades_rcv = (List)results.get("grades");
+            String grade1 = grades_rcv.get(0).toString().substring(grades_rcv.get(0).toString().indexOf("=")+1,grades_rcv.get(0).toString().indexOf("}"));
+            String grade2 = grades_rcv.get(1).toString().substring(grades_rcv.get(1).toString().indexOf("=")+1,grades_rcv.get(1).toString().indexOf("}"));
+            String grade3 = grades_rcv.get(2).toString().substring(grades_rcv.get(2).toString().indexOf("=")+1,grades_rcv.get(2).toString().indexOf("}"));
 
-//            List grade2 = (List)results.get("grades");
-//            List grade = (List) grade2.get(0);
-//            grades.add(Double.parseDouble(grade.get(0).toString()));
-//            grades.add(Double.parseDouble(grade.get(1).toString()));
-//            grades.add(Double.parseDouble(grade.get(2).toString()));
+            grades.add(Double.parseDouble(grade1));
+            grades.add(Double.parseDouble(grade2));
+            grades.add(Double.parseDouble(grade3));
 
             //obj.setId(id);
             obj.setName(name.substring(1,name.length()-1));
             //obj.setName(grades.substring(1,grades.length()-1));
             obj.setClasses(classes.substring(1,classes.length()-1));
-            //obj.setGrades(grades);
+            obj.setGrades(grades);
 
             return obj;
         }
