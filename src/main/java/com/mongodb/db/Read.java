5026a05cd09cb0981d36a64b151ed170a4a5e6b9
jpantonov <jpantonow@gmail.com>
2024-02-08 19:47:41 -0300

    disapproved

--- src/main/java/com/mongodb/db/Read.java
+++ src/main/java/com/mongodb/db/Read.java
@@ -119,6 +119,53 @@ public class Read extends Connection {
             return null;
         }
     }
+    public String read_failed(){
+        try(MongoClient mongoclient = MongoClients.create(Connection.connection)){
+            MongoDatabase sample = mongoclient.getDatabase("mongo_java");
+            MongoCollection<Document> gradesCollection = sample.getCollection("school");
+            String[] gradesFields = {"$grades.1", "$grades.2", "$grades.3"};
+
+            AggregateIterable<Document> disapproved = gradesCollection.aggregate(
+                    Arrays.asList(
+                            Aggregates.unwind("$grades"),
+                            Aggregates.group("$id",
+                                    Accumulators.addToSet("name", "$name"),
+                                    Accumulators.avg("averageGrade1", "$grades.1"),
+                                    Accumulators.avg("averageGrade2", "$grades.2"),
+                                    Accumulators.avg("averageGrade3", "$grades.3")
+                            ),
+                            Aggregates.project(
+                                    Projections.fields(
+                                            Projections.excludeId(),
+                                            Projections.include("name", "grades"),
+                                            Projections.computed("avg",
+                                                    new Document("$round",
+                                                            Arrays.asList(
+                                                                    new Document("$avg",
+                                                                            Arrays.asList("$averageGrade1", "$averageGrade2", "$averageGrade3")
+                                                                    ),
+                                                                    2
+                                                            )
+                                                    )
+                                            )
+                                    )
+                            ),
+                            Aggregates.match(Filters.lt("avg", 5.0))
+                    )
+            );
+
+            String results = null;
+            for(Document doc: disapproved){
+                results += "name:" + doc.get("name").toString() + ",avg:" + doc.get("avg").toString() + "\n";
+            }
+
+            return results;
+        }
+        catch (MongoException e){
+            e.printStackTrace();
+            return null;
+        }
+    }
     public Double readavg(Student obj){
         try(MongoClient mongoclient = MongoClients.create(Connection.connection)){
             MongoDatabase sample = mongoclient.getDatabase("mongo_java");
